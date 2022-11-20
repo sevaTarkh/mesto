2e44b4cf08c490c061685dd54a35aa9dc9c7ca99
seva <tarkhanov2704@mail.ru>
2022-11-20 18:07:26 +0300

    fix bag

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 const popupElement = document.querySelector('.popup');
 const popupEditButton = document.querySelector('.profile__button-edit');
 const popupCloseButton = document.querySelector('.popup__button-close');
-const profile__title = document.querySelector('.profile__title');
-const profile__subtitle = document.querySelector('.profile__subtitle');
-const nameInput = document.querySelector('.form-item_theme_name');
-const jobInput = document.querySelector('.form-item_theme_about');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+const nameInput = document.querySelector('.popup__field_theme_name');
+const jobInput = document.querySelector('.popup__field_theme_about');
 const formElement = document.querySelector('.popup__form');
 
 const closePopup = function() {
@@ -13,14 +13,14 @@ const closePopup = function() {
 
 const openPopup = function() {
     popupElement.classList.add('popup_is-opened');
-    nameInput.value =  profile__title.textContent;
-    jobInput.value =   profile__subtitle.textContent;
+    nameInput.value =  profileTitle.textContent;
+    jobInput.value =   profileSubtitle.textContent;
 };
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    profile__title.textContent =  nameInput.value;
-    profile__subtitle.textContent =  jobInput.value;
+    profileTitle.textContent =  nameInput.value;
+    profileSubtitle.textContent =  jobInput.value;
     closePopup();
 };
 

7db91e4e138e958bb4bdd85380a8990f837407f2
seva <tarkhanov2704@mail.ru>
2022-12-01 19:47:02 +0300

    попытка 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const popup = document.querySelector('.popup');
-const popupEditElement = document.querySelector('.popup_edit');
-const popupAddElement = document.querySelector('.popup_add');
-const popupFotoElement = document.querySelector('.popup_foto');
+const popupEditElement = document.querySelector('.popup_type_edit');
+const popupAddElement = document.querySelector('.popup_type_add');
+const popupFotoElement = document.querySelector('.popup_type_foto');
 const popupEditButton = document.querySelector('.profile__button-edit');
 const popupCloseEditButton = document.querySelector('.popup__button-close_theme_edit');
 const popupCloseAddButton = document.querySelector('.popup__button-close_theme_add');
@@ -54,22 +54,24 @@ function createElement(item){
   const elementDeleteButton = cardElement.querySelector('.element__button-delete');
   const elementFoto = cardElement.querySelector('.element__image');
   const elementTitle = cardElement.querySelector('.element__title');
-  elementDeleteButton.addEventListener('click', handlerDelete);
-  elementLikeButton.addEventListener('click', handlerLike);
+  elementDeleteButton.addEventListener('click', deleteHandler);
+  elementLikeButton.addEventListener('click', likeHandler);
   elementFoto.src = item.link;
+  elementFoto.alt = 'фото' + ' ' + item.name;
   elementTitle.textContent = item.name;
   elementFoto.addEventListener('click', () => openFoto(elementFoto, elementTitle));
   return cardElement;
 };
 const openFoto = function(foto, title){
-  popupFotoElement.classList.add('popup_is-opened');
+  openPopup(popupFotoElement);
   popupFotoName.textContent = title.textContent;
   popupFoto.src = foto.src;
+  popupFoto.alt = 'фото' + ' ' + title.textContent;
 }
-const handlerDelete = (e) => {
+const deleteHandler = (e) => {
   e.target.closest('.element').remove();
 };
-const handlerLike = (e) => {
+const likeHandler = (e) => {
   e.target.classList.toggle('element_is-active');
 };
 const renderElement = (item, wrapElement) =>{
@@ -79,7 +81,7 @@ const renderElement = (item, wrapElement) =>{
 initialCards.forEach(function(item){
   renderElement(item, elementsContainer);
 });
-const formAddSumbitHandler = (e) =>{
+const addSumbitFormHandler = (e) =>{
   e.preventDefault();
   const todo = {
     name: titleInput.value,
@@ -94,26 +96,27 @@ const closePopup = function(item) {
 };
 const openPopup = function(item) {
     item.classList.add('popup_is-opened');
-    if (item === popupEditElement){
-      nameInput.value =  profileTitle.textContent;
-      jobInput.value =   profileSubtitle.textContent;
-    }
 };
-function formEditSubmitHandler (e) {   //отправка формы эдит
+const handleEditOpen = function(){
+  openPopup (popupEditElement)
+    nameInput.value =  profileTitle.textContent;
+    jobInput.value =   profileSubtitle.textContent;
+};
+function editSubmitFormHandler (e) {   //отправка формы эдит
     e.preventDefault();
     profileTitle.textContent =  nameInput.value;
     profileSubtitle.textContent =  jobInput.value;
     closePopup(popupEditElement);
 };
 const reset = function(item){
-  item.reset();
+  item.value='';
 }
 titleInput.addEventListener('click', () => reset(titleInput));
 linkInput.addEventListener('click', () => reset(linkInput));
-formElementEdit.addEventListener('submit', formEditSubmitHandler);// отправка edit формы 
+formElementEdit.addEventListener('submit', editSubmitFormHandler);// отправка edit формы 
 popupCloseEditButton.addEventListener('click', () => closePopup(popupEditElement)); // закрытие формы эдит 
-popupEditButton.addEventListener('click', () => openPopup(popupEditElement));// открытие формы эдит
+popupEditButton.addEventListener('click', handleEditOpen);// открытие формы эдит
 profileAddButton.addEventListener('click', () => openPopup(popupAddElement));// открытие формы адд
 popupCloseAddButton.addEventListener('click', () => closePopup(popupAddElement));//закрытие формы эдит
-formElementAdd.addEventListener('submit', formAddSumbitHandler);
+formElementAdd.addEventListener('submit', addSumbitFormHandler);
 popupCloseFotoButton.addEventListener('click', () => closePopup(popupFotoElement));
